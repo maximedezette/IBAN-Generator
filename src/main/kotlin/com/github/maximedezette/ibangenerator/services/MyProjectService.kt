16dfa613f1dd0da839660d6412908eb02468d324
GitHub Action <action@github.com>
2024-06-09 14:39:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/maximedezette/ibangenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/maximedezette/ibangenerator/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.maximedezette.ibangenerator.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.maximedezette.ibangenerator.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
